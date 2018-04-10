70bfe071ddc25729873820d3e1300702fb07765e
Oliver Hookins <oliver@contentful.com>
2018-04-10 11:01:04 +0200

    Bake version number into binary during build.

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"os"
 )
 
+var gofogVersion = "unknown"
+
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "%s you need to specify a service, available services are: %s\n", os.Args[0], "ec2")
+		fmt.Fprintf(os.Stderr, "%s you need to specify a service, available services are: %s\n", os.Args[0], "ec2, sns")
 		os.Exit(1)
 	}
 	cmd := os.Args[1]
@@ -20,6 +22,9 @@ func main() {
 	case "sns":
 		sns.Run()
 		os.Exit(0)
+    case "-v":
+        fmt.Printf("Gofog version %s\n", gofogVersion)
+        os.Exit(0)
 	}
 	fmt.Sprintf("unknown command %v", cmd)
 	os.Exit(1)
